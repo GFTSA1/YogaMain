3c5f56a29751f1e6dcbf9b45daf50a3feb862cc6
Yar00myr <ivanucaromir@gmail.com>
2026-02-20 00:01:10 +0000

    Fixed the problem with connecting to neon db, wrote 1 endpoint

--- app/database.py
+++ app/database.py
@@ -1,18 +1,26 @@
 import os
+import ssl
 
 from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-from sqlmodel import SQLModel
+from sqlmodel.ext.asyncio.session import AsyncSession
 
 load_dotenv()
 
-DATABASE_URL = os.environ.get("DATABASE_URL").replace(
-    "postgresql://", "postgresql+asyncpg://"
-)
+DATABASE_URL = (
+    os.environ.get("DATABASE_URL").replace("postgresql://", "postgresql+asyncpg://")
+).split("?")[0]
+
+ssl_context = ssl.create_default_context()
+
 engine = create_async_engine(
-    DATABASE_URL, pool_pre_ping=True, echo=True
+    DATABASE_URL, pool_pre_ping=True, connect_args={"ssl": ssl_context}, echo=True
 )  # echo=True for dev
-AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
+AsyncSessionLocal = async_sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False
+)
+
+print(DATABASE_URL)
 
 
 async def get_session():
--- app/routes/courses.py
+++ app/routes/courses.py
@@ -1,15 +1,27 @@
-from typing import Annotated
+from typing import Annotated, List
 from fastapi import APIRouter, Depends, HTTPException, status
+from sqlmodel import select
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from ..database import get_session
 from ..models import YogaCourse
 
+# from ..mock import User
+
 courses_router = APIRouter(prefix="/courses", tags=["Courses"])
 
 
-@courses_router.post("", status_code=status.HTTP_201_CREATED)
-async def create_courses(
-    course: YogaCourse, session: Annotated[AsyncSession, Depends(get_session)]
-):
-    pass
+# @courses_router.post("", response_model=YogaCourse, status_code=status.HTTP_201_CREATED)
+# async def create_courses(
+#     course: YogaCourse, session: Annotated[AsyncSession, Depends(get_session)]
+# ):
+#     # course = course.model_validate()
+
+#     # session.add(course)
+#     pass
+
+
+@courses_router.get("", response_model=List[YogaCourse], status_code=status.HTTP_200_OK)
+async def get_all_courses(session: Annotated[AsyncSession, Depends(get_session)]):
+    courses = (await session.exec(select(YogaCourse))).all()
+    return courses
