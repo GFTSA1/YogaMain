3eccd3a2abdb4dfaa492e79d7de8489174ba67f0
Yar00myr <ivanucaromir@gmail.com>
2026-02-28 16:48:35 +0000

    test for the Trip model

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,4 +1,5 @@
 import pytest
+from datetime import datetime
 from pydantic import ValidationError
 
 from app.models import YogaCourse, Trip
@@ -28,7 +29,7 @@ async def test_yoga_course_default_price(db_session):
     assert course.price == 0.0
 
 
-def test_yoga_course_negative_price():
+async def test_yoga_course_negative_price():
     invalid_data = {
         "name": "Valid Name",
         "description": "Test",
@@ -41,8 +42,54 @@ def test_yoga_course_negative_price():
     assert "Input should be greater than or equal to 0" in str(exc_info.value)
 
 
-def test_yoga_course_name_too_short():
+async def test_yoga_course_name_too_short():
     invalid_data = {"name": "Yo", "description": "Short name test", "level": "Beginner"}
+
+    with pytest.raises(ValidationError) as exc_info:
+        YogaCourse.model_validate(invalid_data)
+
+    assert "String should have at least 3 characters" in str(exc_info.value)
+
+
+async def test_trip_valid_data(db_session):
+    trip = Trip(
+        name="Test Name",
+        description="Test Description",
+        location="Bali",
+        start_date=datetime(2026, 5, 1),
+        end_date=datetime(2026, 5, 10),
+    )
+    db_session.add(trip)
+    await db_session.commit()
+    await db_session.refresh(trip)
+
+    assert trip.id is not None
+    assert trip.name == "Test Name"
+    assert trip.description == "Test Description"
+    assert trip.start_date == datetime(2026, 5, 1)
+
+
+async def test_trip_same_day():
+
+    invalid_data = {
+        "name": "Test Name",
+        "location": "Kyiv",
+        "start_date": datetime(2026, 7, 1),
+        "end_date": datetime(2026, 7, 1),
+    }
+
+    with pytest.raises(ValidationError):
+        Trip.model_validate(invalid_data)
+
+
+async def test_trip_name_too_short():
+    invalid_data = {
+        "name": "Yo",
+        "description": "Short name test",
+        "location": "Kyiv",
+        "start_date": datetime(2026, 7, 1),
+        "end_date": datetime(2026, 7, 4),
+    }
 
     with pytest.raises(ValidationError) as exc_info:
         YogaCourse.model_validate(invalid_data)
