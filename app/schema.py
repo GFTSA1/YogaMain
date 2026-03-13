40d6f4a9c4def10eb961453086b94691cfb59550
Yar00myr <ivanucaromir@gmail.com>
2026-03-13 18:40:29 +0000

    fix: fixed a bug with the course update and carried out a minor code refactor

--- app/schema.py
+++ app/schema.py
@@ -1,8 +1,16 @@
+from typing import Optional
 from sqlmodel import SQLModel, Field
 
 
 class YogaCourseModel(SQLModel):
     name: str = Field(min_length=3)
     description: str
-    price: float = Field(default=0.0, ge=0)
+    price: float = Field(default=5.0, gt=0)
     level: str
+
+
+class YogaCoursePatchModel(SQLModel):
+    name: Optional[str] = Field(default=None, min_length=3)
+    description: Optional[str] = None
+    price: Optional[float] = Field(default=None, gt=0)
+    level: Optional[str] = None
